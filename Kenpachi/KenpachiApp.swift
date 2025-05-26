c4460d0a7be8b28472b83ae50edb8a2c29c8974c
Raghav <raghavmishra1729@outlook.com>
2025-05-26 12:47:54 +0530

    Initial Commit

---
+++ Kenpachi/KenpachiApp.swift
@@ -0,0 +1,17 @@
+//
+//  KenpachiApp.swift
+//  Kenpachi
+//
+//  Created by Raghav Mishra on 26/05/25.
+//
+
+import SwiftUI
+
+@main
+struct KenpachiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
